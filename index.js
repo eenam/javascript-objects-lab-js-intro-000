40aefea59d2a9b085851608af076ae05359b93ab
Emma Nam <en2458@gmail.com>
2020-11-04 03:11:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+//  1) returns an object with the orignal key value pairs and the new key value pair
+//  2) does not modify the original object, but rather returns a clone with the new data
+//  3) returns an object with an updated key value pair
