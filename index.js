e42dd96cf9ee1220746c16b8f6c168891b64f73f
Emma Nam <en2458@gmail.com>
2020-11-04 03:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 //  1) returns an object with the orignal key value pairs and the new key value pair
 //  2) does not modify the original object, but rather returns a clone with the new data
 //  3) returns an object with an updated key value pair
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+// 1) updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object
